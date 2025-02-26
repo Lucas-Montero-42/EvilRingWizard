f1db9108183a650e6701d30f4d373d83e13ced42
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2025-02-26 23:49:55 +0100

    casiiiiiii

--- Evil Ring Wizard/Assets/Code/DungeonGeneration/DungeonGenerator.cs
+++ Evil Ring Wizard/Assets/Code/DungeonGeneration/DungeonGenerator.cs
@@ -29,84 +29,82 @@ public class DungeonGenerator : MonoBehaviour
 
     }
     public GameObject DebugCube;
-    public Room[,] floorPlan = new Room[10, 8];
+    public Room[,] floorPlan = new Room[20, 20];
     [SerializeField]private List<GameObject> roomPrefabs;
     [SerializeField]private Queue<Room> roomQueue = new Queue<Room>();
     int maxrooms = 15;
     int minrooms = 7;
     [SerializeField] int NumberOfRooms;
-    int currentnumberOfRooms = 0;
+    int currentnumberOfRooms = 1;
     public int Level = 1;
-
+    Room startRoom;
     public List<GameObject> enemyPrefabs;
 
     private void Awake()
     {
-        for (int x = 0; x < 9; x++)
+        for (int x = 0; x < 20; x++)
         {
-            for (int y = 0; y < 8; y++)
+            for (int y = 0; y < 20; y++)
             {
                 floorPlan[x, y] = new Room(x,y, false);
             }
         }
         NumberOfRooms = Mathf.Clamp((int)(UnityEngine.Random.Range(0, 2) + 5 + Level * 2.6f), minrooms, maxrooms);
 
-        //Enqueue StartRoom on roomQueue
-        Room startRoom = floorPlan[5, 4];
+        startRoom = floorPlan[10, 10];
         roomQueue.Enqueue(startRoom);
         startRoom.ocupied = true;
         startRoom.roomType = RoomTypes.START;
-
-        for (int i  = 0; i < NumberOfRooms; i++)
+        Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
+        for (int i = 1; i < NumberOfRooms; i++)
         {
-            Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
+            Debug.Log(roomQueue.Peek().roomType);
             CheckAllNeighbours(roomQueue.Dequeue());
-            currentnumberOfRooms++;
-        }
-
-    }
-    private void Start()
-    {
-        for (int x = 0; x < 9; x++)
-        {
-            for (int y = 0; y < 8; y++)
+            if (roomQueue.Peek().roomType == RoomTypes.START)
             {
-                if (floorPlan[x, y].ocupied)
-                {
-                    Instantiate(DebugCube,new Vector3(x * 10 ,0 , y * 10),Quaternion.identity);
-                }
+                i--;
             }
         }
+        Debug.Log(currentnumberOfRooms);
     }
+
     public void CheckAllNeighbours(Room r)
     {
         if (CheckNeighbour(r,Dir.UP))
         {
             GetNeighbour(r, Dir.UP).ocupied = true;
+            GetNeighbour(r, Dir.UP).roomType = RoomTypes.EMPTY;
             roomQueue.Enqueue(GetNeighbour(r, Dir.UP));
-            //Enqueue Room
-            r.roomType = RoomTypes.EMPTY; //DEBUG
+            currentnumberOfRooms++;
+            Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
         }
-        if (CheckNeighbour(r, Dir.DOWN))
+        else if (CheckNeighbour(r, Dir.DOWN))
         {
             GetNeighbour(r, Dir.DOWN).ocupied = true;
+            GetNeighbour(r, Dir.DOWN).roomType = RoomTypes.EMPTY;
             roomQueue.Enqueue(GetNeighbour(r, Dir.DOWN));
-            //Enqueue Room
-            r.roomType = RoomTypes.EMPTY; //DEBUG
+            currentnumberOfRooms++;
+            Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
         }
-        if (CheckNeighbour(r, Dir.LEFT))
+        else if (CheckNeighbour(r, Dir.LEFT))
         {
             GetNeighbour(r, Dir.LEFT).ocupied = true;
+            GetNeighbour(r, Dir.LEFT).roomType = RoomTypes.EMPTY;
             roomQueue.Enqueue(GetNeighbour(r, Dir.LEFT));
-            //Enqueue Room
-            r.roomType = RoomTypes.EMPTY; //DEBUG
+            currentnumberOfRooms++;
+            Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
         }
-        if (CheckNeighbour(r, Dir.RIGHT))
+        else if (CheckNeighbour(r, Dir.RIGHT))
         {
             GetNeighbour(r, Dir.RIGHT).ocupied = true;
+            GetNeighbour(r, Dir.RIGHT).roomType = RoomTypes.EMPTY;
             roomQueue.Enqueue(GetNeighbour(r, Dir.RIGHT));
-            //Enqueue Room
-            r.roomType = RoomTypes.EMPTY; //DEBUG
+            currentnumberOfRooms++;
+            Instantiate(DebugCube, new Vector3(roomQueue.Peek().x * 10, 0, roomQueue.Peek().y * 10), Quaternion.identity);
+        }
+        else
+        {
+            roomQueue.Enqueue(startRoom);
         }
     }
     public bool CheckNeighbour(Room r, Dir d)
@@ -124,8 +122,15 @@ public class DungeonGenerator : MonoBehaviour
 
         System.Random random = new System.Random();
         bool randomBool = random.NextDouble() >= 0.5;
+        bool ocupied = !neighbour.ocupied;
+        bool neighbourneighbour = ocupiedNeighboursNeighbours < 3;
+        bool roomsLeft = currentnumberOfRooms <= NumberOfRooms;
 
-        return !neighbour.ocupied && ocupiedNeighboursNeighbours < 2 && currentnumberOfRooms <= NumberOfRooms && randomBool;
+        if (r.roomType == RoomTypes.START)
+        {
+            return ocupied && neighbourneighbour && roomsLeft;
+        }
+        return ocupied && neighbourneighbour && roomsLeft && randomBool;
     }
     public Room GetNeighbour(Room r, Dir d)
     {
@@ -140,10 +145,10 @@ public class DungeonGenerator : MonoBehaviour
                 neighbour = floorPlan[r.x, r.y-1];
                 break;
             case Dir.LEFT:
-                neighbour = floorPlan[r.x+1, r.y];
+                neighbour = floorPlan[r.x-1, r.y];
                 break;
             case Dir.RIGHT:
-                neighbour = floorPlan[r.x-1, r.y];
+                neighbour = floorPlan[r.x+1, r.y];
                 break;
             default:
                 neighbour = floorPlan[0, 0];
